52729156d605df0c5952373a0b05e51387bf160d
KannoTatsuya5 <1998tatsuya0513@gmail.com>
2024-02-06 13:54:31 +0900

    ユーザー情報を表示,バリデーション付き

---
+++ src/main/java/com/example/exbeginner/controller/Ex04Controller.java
@@ -0,0 +1,38 @@
+package com.example.exbeginner.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.exbeginner.form.UserForm;
+import com.example.exbeginner.model.User;
+
+
+@Controller
+@RequestMapping("/ex04")
+public class Ex04Controller {
+    
+    @GetMapping({"/index","/index/"})
+    public String index(Model model, UserForm userForm) {
+        return "Ex04/exam04";
+    }
+
+    @PostMapping({"/result","/result/"})
+    public String result(@Validated UserForm form, BindingResult result, Model model) {
+        
+        if(result.hasErrors()) {
+            return index(model,form);
+        }
+        User user = new User();
+        user.setName(form.getName());
+        user.setAge(form.getAge());
+        user.setComment(form.getComment());
+        model.addAttribute("user", user);
+        return "Ex04/exam04-result";
+    }
+    
+}
